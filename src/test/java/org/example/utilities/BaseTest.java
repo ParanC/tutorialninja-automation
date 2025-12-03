9b8a0d97ee126c099f24bf9d0f544fa2011597a7
ParanC <94851515+ParanC@users.noreply.github.com>
2025-12-04 00:52:50 +0530

    Enhance WebDriver setup with stability options

--- src/test/java/org/example/utilities/BaseTest.java
+++ src/test/java/org/example/utilities/BaseTest.java
@@ -9,41 +9,68 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 
 import java.io.File;
+import java.time.Duration;
 
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
+
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
-        options.setExperimentalOption("useAutomationExtension", false);
 
+        // extra stability flags
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-background-timer-throttling");
+        options.addArguments("--disable-backgrounding-occluded-windows");
+        options.addArguments("--disable-renderer-backgrounding");
+        options.addArguments("--disable-software-rasterizer");
+        options.addArguments("--disable-features=VizDisplayCompositor,SitePerProcess");
+
+ 
+        options.addArguments("--remote-debugging-port=9222");
+
+        
         String headless = System.getProperty("headless", "false");
-        if (Boolean.parseBoolean(headless)) {
+        boolean isHeadless = Boolean.parseBoolean(headless);
+        if (isHeadless) {
             options.addArguments("--headless=new");
+            
         }
 
+       
         ChromeDriverService service = new ChromeDriverService.Builder()
                 .usingAnyFreePort()
                 .withVerbose(true)
                 .withLogFile(new File("chromedriver.log"))
                 .build();
 
+        
         driver = new ChromeDriver(service, options);
-        driver.manage().window().maximize();
-        driver.get("https://tutorialsninja.com/demo");
+
+      
+        if (!isHeadless) {
+            driver.manage().window().maximize();
+        }
+
+        
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
     @AfterMethod
     public void tearDown() {
-        if (driver != null) driver.quit();
+        if (driver != null) {
+            try {
+                driver.quit();
+            } catch (Exception ignored) {
+            }
+        }
     }
 }
