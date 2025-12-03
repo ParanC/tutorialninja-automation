f51c62030977bba25cd617a98097b689cbe30e7b
ParanC <94851515+ParanC@users.noreply.github.com>
2025-12-04 00:25:36 +0530

    Refactor BaseTest for improved ChromeDriver setup
    Updated the BaseTest class to use ChromeDriverService and added CI-friendly options. Enhanced setup method to navigate to a demo site.

--- src/test/java/org/example/utilities/BaseTest.java
+++ src/test/java/org/example/utilities/BaseTest.java
@@ -1,9 +1,13 @@
+package org.example.utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
 import java.io.File;
 
 public class BaseTest {
@@ -11,12 +15,10 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp() {
-        // setup chromedriver automatically
+
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
-
-        // CI friendly flags
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
@@ -24,15 +26,11 @@ public class BaseTest {
         options.addArguments("--window-size=1920,1080");
         options.setExperimentalOption("useAutomationExtension", false);
 
-        // Use headless only when system property headless=true is passed (workflow does -Dheadless=true)
         String headless = System.getProperty("headless", "false");
         if (Boolean.parseBoolean(headless)) {
-            // try new headless, fallback if needed
             options.addArguments("--headless=new");
-            // if runner fails with new, try: options.addArguments("--headless=chrome");
         }
 
-        // create a service with verbose log to inspect on CI if needed
         ChromeDriverService service = new ChromeDriverService.Builder()
                 .usingAnyFreePort()
                 .withVerbose(true)
@@ -40,61 +38,12 @@ public class BaseTest {
                 .build();
 
         driver = new ChromeDriver(service, options);
-        // maximize or set implicit waits if you want
         driver.manage().window().maximize();
+        driver.get("https://tutorialsninja.com/demo");
     }
 
     @AfterMethod
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        if (driver != null) driver.quit();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// package org.example.utilities;
-
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.chrome.ChromeDriver;
-// import org.testng.annotations.AfterMethod;
-// import org.testng.annotations.BeforeMethod;
-
-// public class BaseTest {
-//     protected WebDriver driver;
-
-//     @BeforeMethod
-//     public void setUp(){
-//         driver =new ChromeDriver();
-//         driver.manage().window().maximize();
-//         driver.get("https://tutorialsninja.com/demo");
-
-//     }
-//     @AfterMethod
-//     public void tearDown(){
-//         if(driver!=null) driver.quit();
-//     }
-// }
